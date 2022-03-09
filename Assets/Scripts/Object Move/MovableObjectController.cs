29989a39b2e665819d9c0e319627dad91dc3c416
eespee <paulsendroiu@gmail.com>
2022-03-09 21:06:49 +0200

    Storage Gun Tester

--- Assets/Scripts/Object Move/MovableObjectController.cs
+++ Assets/Scripts/Object Move/MovableObjectController.cs
@@ -4,12 +4,25 @@ using UnityEngine;
 
 public class MovableObjectController : MonoBehaviour
 {
+    public Vector3 CenterDelta = Vector3.zero;
     public void OnStore()
     {
         gameObject.SetActive(false);
     }
-    public void OnRetrieve()
+    public void OnRetrieve(Vector3 pos)
     {
+        transform.position = pos - CenterDelta;
+        transform.rotation = Quaternion.Euler(0, 0, 0);
         gameObject.SetActive(true);
+    }
+    public bool CanBePlacedThere(Vector3 point)
+    {
+        Ray castRay = new Ray(point - CenterDelta, Vector3.down);
+        return true;// GetComponent<Collider>().Raycast(castRay, out RaycastHit hitInfo, 0);
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(transform.position + CenterDelta,.1f);
     }
 }
--- Assets/Scripts/Object Move/ObjectStorageController.cs
+++ Assets/Scripts/Object Move/ObjectStorageController.cs
@@ -4,20 +4,61 @@ using UnityEngine;
 
 public class ObjectStorageController : MonoBehaviour
 {
+    public float RayRange = 10;
     MovableObjectController stored;
     public void StoreObject(MovableObjectController target)
     {
-        if (stored!=null)
+        if (stored==null)
         {
-            stored.OnStore(false);
+            target.OnStore();
             stored = target;
         }
     }
-    public void TryStoreObjectRay(Ray ray,float range)
+    public void TryStoreObjectRay(Ray ray)
     {
-        if (Physics.Raycast(ray, out RaycastHit hit, range) && hit.transform.TryGetComponent<MovableObjectController>(out MovableObjectController tgtObject))
+        if (Physics.Raycast(ray, out RaycastHit hit, RayRange) && hit.collider.TryGetComponent<MovableObjectController>(out MovableObjectController tgtObject))
         {
             StoreObject(tgtObject);
+
+        }
+    }
+    public void TryRetrieveObject(Vector3 point)
+    {
+        Debug.Log("Try place object " + stored.name + " at point " + point);
+        if (stored.CanBePlacedThere(point))
+        {
+            stored.OnRetrieve(point);
+            stored = null;
+        }
+        else
+        {
+            Debug.Log("Invalid Location");
+        }
+    }
+    protected void Update()
+    {
+        HandlePlayerInput();
+    }
+    protected void HandlePlayerInput()
+    {
+        if (stored == null)
+        {
+            if (Input.GetMouseButton(0))
+        {
+                Ray checkRay = new Ray(transform.position, transform.forward);
+                TryStoreObjectRay(checkRay);
+            }
+        }
+        else
+        {
+            if (Input.GetMouseButtonUp(0))
+            {
+                Ray PlaceRay = new Ray(transform.position, transform.forward);
+                if (Physics.Raycast(PlaceRay, out RaycastHit hit, RayRange))
+                {
+                    TryRetrieveObject(hit.point);
+                }
+            }
         }
     }
 }
