ba6a4f96079d3a34adb53203c3aac72c64e2bf12
eespee <paulsendroiu@gmail.com>
2022-03-11 02:05:15 +0200

    Cute Animation

--- Assets/Scripts/Object Move/MovableObjectController.cs
+++ Assets/Scripts/Object Move/MovableObjectController.cs
@@ -18,25 +18,26 @@ public class MovableObjectController : MonoBehaviour
 
     public void OnStore()
     {
-        gameObject.SetActive(false);
+        if (AnimCoroutine == null)
+        StartCoroutine(SwallowWhole(.25f));
     }
     public void OnRetrieve(Vector3 pos)
     {
         transform.position = pos - CenterDelta;
         transform.rotation = Quaternion.Euler(0, 0, 0);
         gameObject.SetActive(true);
+        if (AnimCoroutine == null)
+            StartCoroutine(SpitOut(.25f));
         if (TryGetComponent(out Rigidbody rigidbody))
         {
             rigidbody.velocity *= 0;
             rigidbody.angularVelocity *= 0;
         }
     }
-    Vector3 lastPos = Vector3.zero;
 
     public bool CanBePlacedThere(Vector3 point)
     {
         Ray castRay = new Ray(point - CenterDelta, Vector3.zero);
-        lastPos = point;
         switch (mesh)
         {
             case CastType.none:
@@ -66,6 +67,67 @@ public class MovableObjectController : MonoBehaviour
     {
         Gizmos.color = Color.red;
         Gizmos.DrawSphere(transform.position + CenterDelta, .1f);
-        
+
+    }
+    Coroutine AnimCoroutine;
+    public IEnumerator SwallowWhole(float time)
+    {
+        Collider c = GetComponent<Collider>();
+        c.enabled = false;
+        Vector3 position = transform.position;
+        Vector3 scale = transform.localScale;
+        if (TryGetComponent(out Rigidbody rigidbody))
+        {
+            rigidbody.isKinematic = true;
+        }
+
+            float StartTime = Time.time + time;
+        while (StartTime > Time.time)
+        {
+            transform.position -= (position - (Camera.main.transform.position + Vector3.down)) * Time.deltaTime / time;
+            transform.localScale -= scale * Time.deltaTime / time  ;
+            yield return new WaitForEndOfFrame();
+        }
+        gameObject.SetActive(false);
+        c.enabled = true;
+        transform.position = position;
+        transform.localScale = scale;
+        AnimCoroutine = null;
+        if (TryGetComponent(out  rigidbody))
+        {
+            rigidbody.isKinematic = false;
+        }
+    }
+    public IEnumerator SpitOut(float time)
+    {
+        Collider c = GetComponent<Collider>();
+        c.enabled = false;
+        Vector3 position = transform.position;
+        Vector3 scale = transform.localScale;
+        if (TryGetComponent(out Rigidbody rigidbody))
+        {
+            rigidbody.isKinematic = true;
+        }
+
+
+        Vector3 origPos = Camera.main.transform.position + Vector3.down;
+       transform.position = origPos;
+        transform.localScale = Vector3.zero;
+
+        float StartTime = Time.time + time;
+        while (StartTime > Time.time)
+        {
+            transform.position += (position - origPos) * Time.deltaTime / time;
+            transform.localScale += scale * Time.deltaTime / time;
+            yield return new WaitForEndOfFrame();
+        }
+        c.enabled = true;
+        transform.position = position;
+        transform.localScale = scale;
+        AnimCoroutine = null;
+        if (TryGetComponent(out rigidbody))
+        {
+            rigidbody.isKinematic = false;
+        }
     }
 }
